12ae9932c234d5d083163a877acca8340e72a3a7
Alicia <aliciabenaventegarcia@gmail.com>
2025-05-29 16:28:52 +0200

    light and shadow working. Test 2

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x666666);
+// scene.background = new THREE.Color(0x666666);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({alpha: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
 // CUBE
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 cube.castShadow = true;
+cube.position.set(1, 2, 1);
 scene.add(cube);
 
 // LIGHT
-const light = new THREE.DirectionalLight(0xffffff, 1, 100);
-light.position.set(0, 1, 1);
+const ambientLight = new THREE.AmbientLight(0x404040);
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(-1, 1, 1);
 light.castShadow = true;
-scene.add(light);
+scene.add(light, ambientLight);
 
 // PLANE
 const planeGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
-const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000});
+const planeMaterial = new THREE.MeshStandardMaterial({ color: 0xff0000, roughness: 0.5, metalness: 1 }); //without roughness and metalness light won't work
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.receiveShadow = true;
 plane.position.set(0, 0, 0);
@@ -34,8 +38,8 @@ scene.add(plane);
 // scene.add(grid);
 
 camera.position.z = 5;
-camera.position.y = -10;
-camera.rotation.x = 1;
+camera.position.y = -3;
+camera.rotation.x = .5;
 
 
 function animate() {
